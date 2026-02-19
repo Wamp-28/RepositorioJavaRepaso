a915b712a2482e894976b67b770f87fb454ade23
AlexM <wmatallana28@gmail.com>
2026-02-19 12:37:18 -0500

    Datos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Alexander Matallana");
-        System.out.println("Zipaquirpa");
+        System.out.println("Zipaquira");
         System.out.println("EDAD: 34");
         System.out.println("PASTOR ALEMAN - SIMONA");
 
